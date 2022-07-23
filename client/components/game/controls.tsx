6ca5af8f175915f23dd0ae4f68500b6a5478643c
Falco Gerritsjans <fcb319@alias.falco.pro>
2022-07-23 20:44:22 +0200

    fix: resolve bug with multiple pointerlock event listeners

--- client/components/game/controls.tsx
+++ client/components/game/controls.tsx
@@ -1,24 +1,26 @@
 import { PointerLockControls } from '@react-three/drei'
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 
 import { GameStateContext } from '../../lib/game/game-state'
 
 const Controls: React.FunctionComponent = () => {
   const { setPointerLock } = useContext(GameStateContext)
 
-  return (
-    <PointerLockControls
-      onLock={(e) => {
-        setPointerLock(true)
-        console.log('lock', e)
-      }}
-      onUnlock={(e) => {
-        setPointerLock(false)
-        console.log('unlock', e)
-      }}
-    />
-
-  )
+  // memo to prevent multiplying listeners
+  return useMemo(() => {
+    return (
+      <PointerLockControls
+        onLock={(e) => {
+          setPointerLock(true)
+          console.log('lock', e)
+        }}
+        onUnlock={(e) => {
+          setPointerLock(false)
+          console.log('unlock', e)
+        }}
+      />
+    )
+  }, [])
 }
 
 export default Controls
